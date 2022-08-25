a9241598d00d05bc43a8b4b9311c1fbef2506ff7
i-ky <gl.ivanovsky@gmail.com>
2022-08-25 22:59:41 +0000

    Add wait() loop

--- src/basset.cpp
+++ src/basset.cpp
@@ -2,6 +2,11 @@
 #include <unistd.h>
 
 #include <sys/ptrace.h>
+#include <sys/wait.h>
+
+#include <iostream>
+
+using std::cerr;
 
 int main(int argc, char *argv[], char *envp[]) {
   argv++;
@@ -12,7 +17,26 @@ int main(int argc, char *argv[], char *envp[]) {
       return -1;
     }
 
-    // TODO wait()
+    int wstatus;
+
+    while (auto pid = wait(&wstatus)) {
+      if (pid == -1) {
+        perror("cannot wait()");
+        return -1;
+      }
+
+      if (WIFEXITED(wstatus) || WIFSIGNALED(wstatus)) {
+        cerr << pid << " exited/terminated by signal\n";
+      } else if (WIFSTOPPED(wstatus)) {
+        cerr << pid << " stopped\n";
+
+        if (ptrace(PTRACE_CONT, pid, nullptr, nullptr) == -1) {
+          perror("cannot ptrace(PTRACE_CONT)");
+        }
+      } else if (WIFCONTINUED(wstatus)) {
+        cerr << pid << " continued\n";
+      }
+    }
 
     return 0;
   }
