40e2cab04209306082497072b39b4fe7411c0d3b
i-ky <gl.ivanovsky@gmail.com>
2022-08-25 22:21:35 +0000

    Implement forking

--- src/basset.cpp
+++ src/basset.cpp
@@ -1,1 +1,29 @@
-int main() { return 0; }
+#include <stdio.h>
+#include <unistd.h>
+
+#include <sys/ptrace.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+  argv++;
+
+  if (auto pid = fork()) {
+    if (pid == -1) {
+      perror("cannot fork()");
+      return -1;
+    }
+
+    // TODO wait()
+
+    return 0;
+  }
+
+  if (ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) == -1) {
+    perror("cannot ptrace(PTRACE_TRACEME)");
+    return -1;
+  }
+
+  execve(*argv, argv, envp);
+  // on success, execve() does not return
+  perror("cannot execve()");
+  return -1;
+}
