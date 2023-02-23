36f52775cb1621175371aa16c33e71796af48216
i-ky <gl.ivanovsky@gmail.com>
2023-02-23 20:52:35 +0000

    Get binary path, working directory and arguments from /proc

--- src/basset.cpp
+++ src/basset.cpp
@@ -4,6 +4,7 @@
 #include <sys/ptrace.h>
 #include <sys/wait.h>
 
+#include <linux/limits.h>
 #include <linux/ptrace.h>
 
 #undef PTRACE_CONT
@@ -11,11 +12,14 @@
 #undef PTRACE_TRACEME
 
 #include <csignal>
+#include <fstream>
 #include <iostream>
 #include <string>
 
 using std::cerr;
+using std::ifstream;
 using std::string;
+using std::to_string;
 
 int main(int argc, char *argv[]) {
   argv++;
@@ -63,8 +67,42 @@ int main(int argc, char *argv[]) {
 
         if (WSTOPSIG(wstatus) == SIGTRAP) {
           switch (wstatus >> 16) {
+          case PTRACE_EVENT_EXEC: {
+            char exe[PATH_MAX];
+            auto ret = readlink(("/proc/" + to_string(pid) + "/exe").c_str(),
+                                exe, sizeof(exe));
+
+            if (ret == -1) {
+              perror("cannot readlink(\"/proc/[pid]/exe\")");
+              return -1;
+            }
+
+            cerr << string(exe, ret) << '\n';
+
+            char cwd[PATH_MAX];
+            ret = readlink(("/proc/" + to_string(pid) + "/cwd").c_str(), cwd,
+                           sizeof(cwd));
+
+            if (ret == -1) {
+              perror("cannot readlink(\"/proc/[pid]/cwd\")");
+              return -1;
+            }
+
+            cerr << string(cwd, ret) << '\n';
+
+            ifstream cmdline("/proc/" + to_string(pid) + "/cmdline");
+
+            for (string arg; getline(cmdline, arg, '\0');) {
+              cerr << '\t' << arg.data() << '\n';
+            }
+
+            if (!cmdline.eof()) {
+              cerr << "failed to read /proc/[pid]/cmdline\n";
+            }
+
+            break;
+          }
           case PTRACE_EVENT_CLONE:
-          case PTRACE_EVENT_EXEC:
           case PTRACE_EVENT_FORK:
           case PTRACE_EVENT_VFORK:
             break;
